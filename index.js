3385278a51ee6a3971bc1edda1023a3c4be3321a
Radek Ripa <ripa.r@seznam.cz>
2022-11-13 23:03:26 +0100

    added intToBlocks added intToBits

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-
+/**
+ * function converst string hex value to integer
+ * @param {string} hexValue 
+ * @returns int value of hex
+ */
 const hexToInt = (hexValue) => {
   return parseInt(hexValue, 16);
 }
 
+/**
+ * function converst int to hex string
+ * @param {integer} intValue 
+ * @returns return string hex value
+ */
+const intToHex = (intValue) => {
+  return intValue.toString(16).toUpperCase();
+}
+
+/**
+ * function convert int value intValue to nBits array of bits 
+ * @param {integer} intValue 
+ * @param {integer} nBits 
+ * @returns array of bits
+ */
 const intToBits = (intValue, nBits = 32) => [...Array(nBits)].map((x, i) => (intValue >> i) & 1);
 
-const hexconversions = { hexToInt: hexToInt, intToBits: intToBits };
+const intToBlocks = (intValue, startBit, blockSize) => ((intValue >> startBit) & (Math.pow(2, blockSize) - 1))
+
+const hexconversions = { hexToInt: hexToInt, intToHex: intToHex, intToBits: intToBits, intToBlocks: intToBlocks };
 
 module.exports = hexconversions;
--- tester/index.js
+++ tester/index.js
@@ -1,2 +1,9 @@
 const hexconversions = require("hexconversions");
 console.log(hexconversions.hexToInt("0x123"));
+
+console.log(hexconversions.intToBits(123));
+
+
+console.log(hexconversions.intToHex(123));
+
+console.log(hexconversions.intToBlocks(123, 2, 3));
