2f6cffddb7d1e0624220cc41e96701b206d70438
Radek Ripa <ripa.r@seznam.cz>
2022-11-14 23:28:35 +0100

    adding more block related functions adding testing routines for nbew functions

--- index.js
+++ index.js
@@ -24,8 +24,43 @@ const intToHex = (intValue) => {
  */
 const intToBits = (intValue, nBits = 32) => [...Array(nBits)].map((x, i) => (intValue >> i) & 1);
 
-const intToBlocks = (intValue, startBit, blockSize) => ((intValue >> startBit) & (Math.pow(2, blockSize) - 1))
+/**
+ * 
+ * @param {integer} intValue input value to be filtered
+ * @param {integer} startBit which bit to start filter 
+ * @param {integer} blockSize how many bits to filter
+ * @returns 
+ */
+const intToBlock = (intValue, startBit, blockSize) => ((intValue >> startBit) & (Math.pow(2, blockSize) - 1))
 
-const hexconversions = { hexToInt: hexToInt, intToHex: intToHex, intToBits: intToBits, intToBlocks: intToBlocks };
+/*
+  blockArray = [startBit: 0, blockSize:1 ]
+*/
+
+/**
+ * Take hex value in int. And for each block parse the value. Store back to block array
+ * @param {integer} intValue - hex value
+ * @param {Array} blockArray - array of object with block descriptions ex: blockArray = [startBit: 0, blockSize:1 ]
+ * @returns return array of blocks ex blockArray = [startBit: 0, blockSize:1, intValue: 1] if int value have 1 on bit 0
+ */
+const intToBlockObjectArray = (intValue, blockArray) => blockArray.map(x => ({ ...x, intValue: intToBlock(intValue, x.startBit, x.blockSize) }))
+
+/**
+ * convert block to integer value
+ * @param {integer} blockValue value in block
+ * @param {integer} startBit start bit of block
+ * @param {integer} blockSize size of block
+ * @returns integer with block value on correct place
+ */
+const blockToInt = (blockValue, startBit, blockSize) => ((blockValue & (Math.pow(2, blockSize) - 1)) << startBit);
+
+/**
+ * function take blockArray and create int from it
+ * @param {Array} blockArray array of block objects in this format [{ startBit: 1, blockSize: 2, intValue: 2 },...]
+ * @returns retunr integer form block
+ */
+const blockObjectArrayToInt = (blockArray) => blockArray.reduce((value, currentBlock) => (value | blockToInt(currentBlock.intValue, currentBlock.startBit, currentBlock.blockSize)), 0);
+
+const hexconversions = { hexToInt: hexToInt, intToHex: intToHex, intToBits: intToBits, intToBlock: intToBlock, blockToInt: blockToInt, intToBlockObjectArray: intToBlockObjectArray, blockObjectArrayToInt: blockObjectArrayToInt };
 
 module.exports = hexconversions;
--- tester/index.js
+++ tester/index.js
@@ -6,4 +6,16 @@ console.log(hexconversions.intToBits(123));
 
 console.log(hexconversions.intToHex(123));
 
-console.log(hexconversions.intToBlocks(123, 2, 3));
+console.log(hexconversions.intToBlock(123, 2, 3));
+
+console.log(hexconversions.blockToInt(3, 2, 3));
+
+const blockArray = [{ startBit: 1, blockSize: 2 }, { startBit: 3, blockSize: 2 }];
+console.log(hexconversions.intToBlockObjectArray(123, blockArray));
+hexconversions.intToBlockObjectArray(123, blockArray).forEach(element => {
+  console.log(element);
+});
+
+const blockArrayWithInt = [{ startBit: 1, blockSize: 2, intValue: 2 }, { startBit: 3, blockSize: 2, intValue: 3 }];
+
+console.log(hexconversions.blockObjectArrayToInt(blockArrayWithInt));
